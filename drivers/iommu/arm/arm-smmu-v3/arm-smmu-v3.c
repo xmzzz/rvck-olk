1bff963df1ee22d6f6706094514e22859629e0c0
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2024-02-16 15:40:25 +0100

    iommu: constify of_phandle_args in xlate
    mainline inclusion
    from Linux 6.8-rc5
    commit b42a905b6aad40c092cf17f4b295a4c389bc7206
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/78
    
    --------------------------------
    
    The xlate callbacks are supposed to translate of_phandle_args to proper
    provider without modifying the of_phandle_args.  Make the argument
    pointer to const for code safety and readability.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Gao Rui <gao.rui@zte.com.cn>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -3924,7 +3924,8 @@ static int arm_smmu_clear_dirty_log(struct iommu_domain *domain,
 }
 #endif
 
-static int arm_smmu_of_xlate(struct device *dev, struct of_phandle_args *args)
+static int arm_smmu_of_xlate(struct device *dev,
+			     const struct of_phandle_args *args)
 {
 	return iommu_fwspec_add_ids(dev, args->args, 1);
 }
